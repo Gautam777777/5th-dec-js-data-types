0c1420745c2e01da81b42b2b47f96a02e4bd076f
gautam777777 <gautamsharma0057@gmail.com>
2022-12-05 23:33:29 +0530

    js data types

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Single Line Comment
+
+//Lets take a Js Object for storing student information
+
+var student_name = "Gautam";
+var student_Surname = "Sharma";
+
+
+//Js Object
+
+// lets store the student1 information inside a js object
+
+var student1 = {
+    // 1. property:value
+
+    name:"sampat",
+    surname:"prajapati"
+
+    // 2. .....
+};
+
+// 1st way to see the js code output
+
+
+// object.propertyname
+console.log("Hello "+student1.name); // + is a concatination operator
+
---
+++ js/script2.js
@@ -0,0 +1,21 @@
+// lets store the student3 information inside a js object
+
+const student4 = {
+    // 1. property:value
+    name:"suryabhan",
+    surname:"singh"
+
+    // 2. .....
+};
+
+
+// 4th way to see the js code output
+
+//object.propertyname
+
+document.write('<div>'+student4.name+'</div>');
+
+
+
+
+
